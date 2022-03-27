89bd5758decdf248870e74c32db922abcf0aaada
gigi-luna <98123241+gigi-luna@users.noreply.github.com>
2022-03-26 20:03:57 -0400

    Add files via upload

---
+++ Design Pattern/Part 2/VisitorPatterDemo.java
@@ -0,0 +1,8 @@
+public class VisitorPatterDemo {
+    public static void main(String[] args){
+
+        ComputerPart computer = new Computer();
+        computer.accept(new ComputerPartDisplayVisitor());
+    }
+    
+}
